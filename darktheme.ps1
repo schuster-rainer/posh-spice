381131fd1253c0c59347f660343d02943970d0c7
Rainer Schuster <rainer.schuster@clondiag.com>
2010-02-04 16:13:15 +0100

    modified darktheme to 'purple haze'

--- darktheme.ps1
+++ darktheme.ps1
@@ -5,18 +5,18 @@ $psISE.Options.FontName = 'Consolas'
 $psISE.Options.FontSize = 16
 
 # output pane
-$psISE.Options.OutputPaneBackgroundColor = '#FF151515'
-$psISE.Options.OutputPaneTextBackgroundColor = '#FF151515'
+$psISE.Options.OutputPaneBackgroundColor = '#FF272727'
+$psISE.Options.OutputPaneTextBackgroundColor = '#FF272727'
 $psISE.Options.OutputPaneForegroundColor = '#FFFFFFFF'
 
 # command pane
-$psISE.Options.CommandPaneBackgroundColor = '#FF151515'
+$psISE.Options.CommandPaneBackgroundColor = '#FF272727'
 
 # script pane
-$psISE.Options.ScriptPaneBackgroundColor = '#FF151515'
+$psISE.Options.ScriptPaneBackgroundColor = '#FF272727'
 
 # tokens
-$psISE.Options.TokenColors['Command'] = '#FFFFFF60'
+$psISE.Options.TokenColors['Command'] = '#FFFFFFAF'
 $psISE.Options.TokenColors['Unknown'] = '#FFFFFFFF'
 $psISE.Options.TokenColors['Member'] = '#FFFFFFFF'
 $psISE.Options.TokenColors['Position'] = '#FFFFFFFF'
@@ -25,12 +25,12 @@ $psISE.Options.TokenColors['GroupStart'] = '#FFFFFFFF'
 $psISE.Options.TokenColors['LineContinuation'] = '#FFFFFFFF'
 $psISE.Options.TokenColors['NewLine'] = '#FFFFFFFF'
 $psISE.Options.TokenColors['StatementSeparator'] = '#FFFFFFFF'
-$psISE.Options.TokenColors['Comment'] = '#FFAEAEAE'
-$psISE.Options.TokenColors['String'] = '#FF00D42D'
-$psISE.Options.TokenColors['Keyword'] = '#FFFFDE00'
+$psISE.Options.TokenColors['Comment'] = '#FF808080'
+$psISE.Options.TokenColors['String'] = '#FF8080B0'
+$psISE.Options.TokenColors['Keyword'] = '#FF8080B0'
 $psISE.Options.TokenColors['Attribute'] = '#FF84A7C1'
-$psISE.Options.TokenColors['Type'] = '#FF84A7C1'
-$psISE.Options.TokenColors['Variable'] = '#FF00D42D'
-$psISE.Options.TokenColors['CommandParameter'] = '#FFFFDE00'
+$psISE.Options.TokenColors['Type'] = '#FF8080B0'
+$psISE.Options.TokenColors['Variable'] = '#FFDCDCCC'
+$psISE.Options.TokenColors['CommandParameter'] = '#FF8080B0'
 $psISE.Options.TokenColors['CommandArgument'] = '#FFFFFFFF'
-$psISE.Options.TokenColors['Number'] = '#FF98FE1E'
+$psISE.Options.TokenColors['Number'] = '#FF8CD0D3'
